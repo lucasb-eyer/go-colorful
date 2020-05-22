2a939b416ef3325557838602eda58f4f65b8507a
Alik Khilazhev <alikhil@mail.ru>
2020-05-22 17:19:34 +0300

    Add envconfig deserialization

--- hexcolor.go
+++ hexcolor.go
@@ -55,3 +55,13 @@ func (hc *HexColor) UnmarshalJSON(data []byte) error {
 func (hc HexColor) MarshalJSON() ([]byte, error) {
 	return json.Marshal(Color(hc).Hex())
 }
+
+// Decode - deserilize function for https://github.com/kelseyhightower/envconfig
+func (hc *HexColor) Decode(hexCode string) error {
+	var col, err = Hex(hexCode)
+	if err != nil {
+		return err
+	}
+	*hc = HexColor(col)
+	return nil
+}
