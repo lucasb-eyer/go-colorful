8ac5283df1de4c96f9a62a3772f9303f37b619b4
lublak <44057030+lublak@users.noreply.github.com>
2024-10-24 20:57:21 +0200

    implement functions with custom rand handler

--- colorgens_test.go
+++ colorgens_test.go
@@ -10,23 +10,43 @@ import (
 
 // Check if it returns all valid colors.
 func TestColorValidity(t *testing.T) {
-	seed := time.Now().UTC().UnixNano()
-	rand.Seed(seed)
-
+	// with default seed
 	for i := 0; i < 100; i++ {
 		if col := WarmColor(); !col.IsValid() {
-			t.Errorf("Warm color %v is not valid! Seed was: %v", col, seed)
+			t.Errorf("Warm color %v is not valid! Seed was: default", col)
 		}
 
 		if col := FastWarmColor(); !col.IsValid() {
-			t.Errorf("Fast warm color %v is not valid! Seed was: %v", col, seed)
+			t.Errorf("Fast warm color %v is not valid! Seed was: default", col)
 		}
 
 		if col := HappyColor(); !col.IsValid() {
-			t.Errorf("Happy color %v is not valid! Seed was: %v", col, seed)
+			t.Errorf("Happy color %v is not valid! Seed was: default", col)
 		}
 
 		if col := FastHappyColor(); !col.IsValid() {
+			t.Errorf("Fast happy color %v is not valid! Seed was: default", col)
+		}
+	}
+
+	// with custom seed
+	seed := time.Now().UTC().UnixNano()
+	rand := rand.New(rand.NewSource(seed))
+
+	for i := 0; i < 100; i++ {
+		if col := WarmColorWithRand(rand); !col.IsValid() {
+			t.Errorf("Warm color %v is not valid! Seed was: %v", col, seed)
+		}
+
+		if col := FastWarmColorWithRand(rand); !col.IsValid() {
+			t.Errorf("Fast warm color %v is not valid! Seed was: %v", col, seed)
+		}
+
+		if col := HappyColorWithRand(rand); !col.IsValid() {
+			t.Errorf("Happy color %v is not valid! Seed was: %v", col, seed)
+		}
+
+		if col := FastHappyColorWithRand(rand); !col.IsValid() {
 			t.Errorf("Fast happy color %v is not valid! Seed was: %v", col, seed)
 		}
 	}
